aedc64a219fc4d93cffecf562870862537a26576
Holden Lee <htlee1500@gmail.com>
2024-07-23 14:10:04 -0700

    Added folder for storing learning rules under /agents. Added a simple Q-learning rule (no idea if it will work).

---
+++ agents/learning_rules/simple_Q.py
@@ -0,0 +1,49 @@
+import numpy
+
+class Simple_Q:
+
+        # Simple_Q defines the value of a state
+        # as the amount of food collected as a
+        # function of the number of steps.
+        # Using an optimal solution to the
+        # SF trail, Simple_Q computes the
+        # mean squared error of the given
+        # agent's path vs the optimal solution.
+        
+        def __init__(self):
+
+                loader = np.load("simple_Q.npz", allow_pickle = True)
+
+                step = loader['step']
+                food = loader['food']
+
+                total_steps = np.shape(step)[0]
+                
+                
+                self.optimal = dict()
+                self.optimal_max = food[total_steps-1]
+
+                for i in range(total_steps):
+
+                        self.optimal.update({step[i] : food[i]})
+
+
+
+        def get_Q(self, food, steps):
+                
+                if steps in self.optimal.keys():
+
+                        optimalQ = self.optimal.get(steps)
+
+                else:
+                        optimalQ = self.optimal_max
+
+
+                experimentQ = food
+
+                loss = (optimalQ - experimentQ)**2
+
+                return loss
+
+                
+                
