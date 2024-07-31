08a0b635687642ee4ed99be4d4c0ff1ef12d9567
Holden Lee <htlee1500@gmail.com>
2024-07-31 11:42:40 -0700

    Added player using Fractional LIF-based network

--- agents/learning_rules/STDP.py
+++ agents/learning_rules/STDP.py
@@ -16,8 +16,8 @@ class Learner():
                 self.elig_output = torch.zeros((num_output, num_hidden)).to(device)
 
                 # eligibility timers for elig. trace values
-                self.eh_timer = torch.zeros_like(self.elig_hidden)
-                self.eo_timer = torch.zeros_like(self.elig_output)
+                self.eh_timer = torch.zeros_like(self.elig_hidden).to(device)
+                self.eo_timer = torch.zeros_like(self.elig_output).to(device)
 
                 self.num_input = num_input
                 self.num_hidden = num_hidden
@@ -25,14 +25,16 @@ class Learner():
 
 
                 # weight_modifiers
-                self.weight_mod_hidden = torch.ones_like(self.elig_trace_hidden).to(device)
-                self.weight_mod_output = torch.ones_like(self.elig_trace_output).to(device)
+                self.weight_mod_hidden = torch.ones_like(self.elig_hidden).to(device)
+                self.weight_mod_output = torch.ones_like(self.elig_output).to(device)
 
 
                 self.w_s_max = w_s_max
                 self.w_inc_hid = w_inc_hid
                 self.w_inc_out = w_inc_out
 
+                self.density_threshold = 0.1
+                
 
         # Calculates eligibility traces
         def update(self, in_spk, hid_spk, out_spk):
@@ -40,18 +42,18 @@ class Learner():
                 # for input-hidden trace, increase eligibility by
                 # [self.window] for indices [i,j] where
                 # in_spk[i] and hid_spk[j] are both 1
-                new_elig_hidden = torch.mul(in_spk, torch.reshape(hid_spk, (self.num_hidden, self.num_input))) * window
+                new_elig_hidden = torch.mul(torch.reshape(in_spk, (1, self.num_input)), torch.reshape(hid_spk, (self.num_hidden, 1))) * self.window
 
 
                 # Do the same for hidden-output trace
-                new_elig_output = torch.mul(hid_spk, torch.reshape(out_spk, (self.num_output, self.num_hidden))) * window
+                new_elig_output = torch.mul(torch.reshape(hid_spk, (1, self.num_hidden)), torch.reshape(out_spk, (self.num_output, 1))) * self.window
 
 
                 # Then traces need to be decayed and regularized;
                 # Traces decay by 1 at each step (each call of update())
                 # Only decay positive-valued indices
-                decay_hidden = (self.eh_timer > 0)
-                decay_output = (self.eo_timer > 0)
+                decay_hidden = (self.eh_timer > 0).float()
+                decay_output = (self.eo_timer > 0).float()
 
                 self.eh_timer.sub_(decay_hidden)
                 self.eo_timer.sub_(decay_output)
@@ -62,29 +64,35 @@ class Learner():
 
 
                 # Trace values also need to stay between 0 and self.window
-                regularization_hidden = torch.zeros(0)
-                regularization_output = torch.zeros(0)
+                regularization_hidden = torch.zeros(0).to(self.device)
+                regularization_output = torch.zeros(0).to(self.device)
 
-                torch.remainder(self.delta_elig_hidden, self.window, out=regularization_hidden)
-                torch.remainder(self.delta_elig_output, self.window, out=regularization_output)
+                torch.remainder(new_elig_hidden, self.window, out=regularization_hidden)
+                torch.remainder(new_elig_output, self.window, out=regularization_output)
 
                 self.eh_timer.sub_(regularization_hidden)
                 self.eo_timer.sub_(regularization_output)
 
                 # eligible synapses are those with timers > 0
-                self.elig_hidden = (self.eh_timer > 0)
-                self.elig_output = (self.eo_timer > 0)
+                self.elig_hidden = (self.eh_timer > 0).float()
+                self.elig_output = (self.eo_timer > 0).float()
 
 
         
         def weight_change(self, criticism):
 
-                if criticism > 0:
+                sensitivity_hidden = self.elig_hidden.sum().item() / (self.num_hidden * self.num_input)
+                sensitivity_output = self.elig_output.sum().item() / (self.num_hidden * self.num_output)
+
+
+
+                # If the critic returns a negative, we've performed badly.
+                if criticism < 0:
                         
                         delta_w_s_hidden = torch.mul(self.weight_mod_hidden, criticism*self.w_inc_hid)
 
                         delta_w_s_output = torch.mul(self.weight_mod_output, criticism*self.w_inc_out)
-                        
+                
                 else:
                         inverse = torch.sub(1, self.weight_mod_hidden)
                         delta_w_s_hidden = torch.mul(inverse, criticism*self.w_inc_hid)
@@ -92,10 +100,25 @@ class Learner():
                         inverse = torch.sub(1, self.weight_mod_output)
                         delta_w_s_output = torch.mul(inverse, criticism*self.w_inc_out)
 
+                delta_w_s_hidden.div_(self.w_s_max)
+                delta_w_s_output.div_(self.w_s_max)
                         
                 self.weight_mod_hidden.add_(delta_w_s_hidden.mul(self.elig_hidden))
                 self.weight_mod_output.add_(delta_w_s_output.mul(self.elig_output))
 
 
+                weight_hid = self.weight_mod_hidden
+                weight_out = self.weight_mod_output
+
+                
+                if sensitivity_hidden < self.density_threshold:
+
+                        weight_hid = 1.10
+
+                if sensitivity_output < self.density_threshold:
+
+                        weight_out = 1.10
+                
+
                 # synapses should be multiplied by these values
-                return self.weight_mod_hidden, self.weight_mod_output
+                return weight_hid, weight_out
